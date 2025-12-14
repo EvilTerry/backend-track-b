4eaee12f0f5bdf32f44e8255d376f6d9e0eac4f3
Egidijus <evilterixyoutub@gmail.com>
2025-12-14 16:00:42 +0100

    Initialize Express server with database connection and error handling

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import { initDb } from './database/database';
+import { errorHandler } from './middleware/errorHandler';
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+// app.use('/api', productRoute);
+
+app.use(errorHandler);
+
+initDb().then(() =>{
+    app.listen(PORT, () => {
+        console.log(`Server running at http://localhost:${PORT}`);
+    });
+});
