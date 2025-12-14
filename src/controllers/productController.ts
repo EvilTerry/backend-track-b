42a4a671908a05fb18fbeab832f343f312fd7ae7
Egidijus <evilterixyoutub@gmail.com>
2025-12-14 16:14:33 +0100

    Implement getProducts function to retrieve all products from the database

--- src/controllers/productController.ts
+++ src/controllers/productController.ts
@@ -1,5 +1,16 @@
 import { Request, Response, NextFunction } from 'express';
+import { getDB } from '../database/database';
 
 export const getProducts = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const db = await getDB();
+        
+        const products = await db.all('SELECT * FROM products');
 
+        res.json({
+            data: products
+        });
+    } catch (error) {
+        next(error);
+    }
 };
